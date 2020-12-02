874edb4e377e4e436341242934536a7198f6bfb8
Jimmy Egberts <jimmyegberts@hotmail.com>
2020-12-02 20:38:40 +0100

    Fix spacing and SQL formatting

--- include/whisp-server/db.h
+++ include/whisp-server/db.h
@@ -7,16 +7,17 @@
 #include <string>
 
 namespace db {
-extern sqlite3 *conn;
+  extern sqlite3 *conn;
 
-void init_database(std::string sqlite_path);
-void close_database();
+  void init_database(std::string sqlite_path);
+  void close_database();
 
-namespace user {
-RegisteredUser *add(std::string username, std::string email,
-                    std::string password);
-RegisteredUser *get(std::string username);
-}
+  namespace user {
+    RegisteredUser *add(std::string username, std::string email,
+                        std::string password);
+    RegisteredUser *get(std::string username);
+  }
+
   namespace channel {
     Channel *add(std::string name, int owner_id, int max_users);
     Channel *get(std::string name);
