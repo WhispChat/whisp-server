3984a868b328b348599d9077b0f2324492bd17e1
Jimmy Egberts <jimmyegberts@hotmail.com>
2020-12-02 21:05:59 +0100

    Modify admin account to work with new password hash system

--- sql/whisp.sql
+++ sql/whisp.sql
@@ -13,7 +13,9 @@ CREATE TABLE IF NOT EXISTS "channels" (
     "owner_id" INTEGER NOT NULL,
     "max_users" INTEGER NOT NULL
 );
-INSERT INTO users (id, username, email, password) VALUES (NULL, 'admin', 'admin@whisp.dev', 'password123');
+INSERT INTO users (id, username, email, password_hash, password_salt) VALUES (NULL, 'admin', 'admin@whisp.dev',
+                                                                              'fad24daa6c774da1e47c0d0c0af525ad17a3821b4a528ce5047f2cdf4baae19',
+                                                                              'c7d0111374bec5b5b42cf08a6a8a30ea8ca75314177bd7a04fd61221698a6277');
 INSERT INTO channels (id, name, owner_id, max_users) VALUES (NULL, 'general', 0, 256);
 DELETE FROM sqlite_sequence;
 COMMIT;
